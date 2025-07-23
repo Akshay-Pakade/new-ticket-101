46b35ea002006c61f5592e127275f53d8742819d
Akshay Pakade <pakadeakshay@gmail.com>
2025-07-23 09:54:04 +0530

    new code for rg added

--- main.tf
+++ main.tf
@@ -3,9 +3,9 @@ resource "azurerm_resource_group" "demo_rg1" {
   location = "East US"
 }
 
-resource "azurerm_resource_group" "demo_rg2" {
-  name     = "demo-rg2"
-  location = "East US"
+resource "azurerm_resource_group" "akshay_rg2" {
+  name     = "akshay-rg2"
+  location = "west europe"
 }
 
 resource "azurerm_resource_group" "demo_rg3" {
